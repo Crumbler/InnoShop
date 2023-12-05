d29f57dccbbc558b2579f78b4ebbe644acd5e63a
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-05 22:46:52 +0300

    Add NotFound and UserNotFound domain exceptions

---
+++ UserService/Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace UserService.Domain.Exceptions
+{
+    public class UserNotFoundException(int userId) : 
+        NotFoundException($"The user with the identifier {userId} was not found.")
+    { }
+}
