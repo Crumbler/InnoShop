93e44fb5519427c5cd206183469a5c12eb6d05c8
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-07 19:11:25 +0300

    Create UserController

---
+++ UserService/Presentation/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Application.DTOs;
+using UserService.Application.Interfaces;
+using UserService.Domain.Exceptions;
+
+namespace UserService.Presentation.Controllers
+{
+    [ApiController]
+    [Route("Users")]
+    public class UserController(IUserService userService) : Controller
+    {
+        [HttpGet("{id:int}")]
+        [ProducesResponseType<string>(StatusCodes.Status404NotFound)]
+        [ProducesResponseType<UserDTO>(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetUser([FromRoute] int id)
+        {
+            try
+            {
+                return Ok(await userService.GetUserAsync(id));
+            }
+            catch (UserNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
