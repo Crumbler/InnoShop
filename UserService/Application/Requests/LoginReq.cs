566178fdfb82dfa570ec6d6c4abf71ed7fa039d6
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-10 00:28:19 +0300

    Create login request class

---
+++ UserService/Application/Requests/LoginReq.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Application.Requests
+{
+    public class LoginReq
+    {
+        [EmailAddress]
+        [StringLength(maximumLength: 30, MinimumLength = 8)]
+        public required string Email { get; set; }
+
+        [StringLength(maximumLength: 30, MinimumLength = 8)]
+        [RegularExpression(@"^(?=\d*[a-zA-Z]+)(?=[a-zA-Z]*\d+).*$",
+            ErrorMessage = "The password must contain only letters of the English alphabet and digits")]
+        public required string Password { get; set; }
+    }
+}
