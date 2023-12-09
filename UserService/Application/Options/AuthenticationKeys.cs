3e6a4e87e976274ddb70e62ce0a96a1162ceb087
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-10 00:43:46 +0300

    Add authentication keys and options

---
+++ UserService/Application/Options/AuthenticationKeys.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+
+namespace UserService.Application.Options
+{
+    public class AuthenticationKeys
+    {
+        public RsaSecurityKey PrivateKey { get; }
+        public RsaSecurityKey PublicKey { get; }
+
+        public AuthenticationKeys(AuthenticationOptions options)
+        {
+            using var rsa = RSA.Create();
+
+            rsa.ImportRSAPrivateKey(source: Convert.FromBase64String(options.RsaPrivateKey), out _);
+
+            PrivateKey = new RsaSecurityKey(rsa);
+
+            rsa.ImportRSAPublicKey(source: Convert.FromBase64String(options.RsaPublicKey), out _);
+
+            PublicKey = new RsaSecurityKey(rsa);
+        }
+    }
+}
---
+++ UserService/Application/Options/AuthenticationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace UserService.Application.Options
+{
+    public class AuthenticationOptions
+    {
+        public const string Authentication = "Authentication";
+        public required string RsaPrivateKey { get; set; }
+        public required string RsaPublicKey { get; set; }
+    }
+}
