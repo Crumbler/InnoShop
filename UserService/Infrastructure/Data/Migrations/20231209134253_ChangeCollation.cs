067fbeff73ceb24ba17b12cae6538d193b6833f9
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-09 16:54:30 +0300

    Use the "Latin1_General_100_CS_AS_KS_WS_SC" collation and migrate

---
+++ UserService/Infrastructure/Data/Migrations/20231209134253_ChangeCollation.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                collation: "Latin1_General_100_CS_AS_KS_WS_SC");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varchar(max)",
+                unicode: false,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varchar(max)",
+                unicode: false,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                oldCollation: "Latin1_General_100_CS_AS_KS_WS_SC");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldUnicode: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(max)",
+                oldUnicode: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20);
+        }
+    }
+}
--- UserService/Infrastructure/Data/UserServiceDbContext.cs
+++ UserService/Infrastructure/Data/UserServiceDbContext.cs
@@ -8,5 +8,10 @@ namespace UserService.Infrastructure.Data
     {
         public DbSet<EFRole> Roles { get; set; }
         public DbSet<EFUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Latin1_General_100_CS_AS_KS_WS_SC");
+        }
     }
 }
