2eb5b8eccaa2209cb88d5246b4a8fc429fb4c20d
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-10 14:42:01 +0300

    Add InvalidCredentials exception

---
+++ UserService/Domain/Exceptions/InvalidCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace UserService.Domain.Exceptions
+{
+    public class InvalidCredentials : UnauthorizedException
+    {
+        public InvalidCredentials() : base("The specified credentials are invalid") { }
+    }
+}
---
+++ UserService/Domain/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,7 @@
+﻿namespace UserService.Domain.Exceptions
+{
+    public class UnauthorizedException : Exception
+    {
+        protected UnauthorizedException(string message) : base(message) { }
+    }
+}
