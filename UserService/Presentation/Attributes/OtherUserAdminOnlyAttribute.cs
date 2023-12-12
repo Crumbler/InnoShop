ca97ec0d2852bc1ae719948ef62dde835c2b0a44
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-12 16:05:19 +0300

    Add OtherUserAdminOnly attribute

---
+++ UserService/Presentation/Attributes/OtherUserAdminOnlyAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using UserService.Domain.Exceptions;
+
+namespace UserService.Presentation.Attributes
+{
+    /// <summary>
+    /// Ensures that only users with admin privileges can affect other users
+    /// </summary>
+    public class OtherUserAdminOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var identity = (ClaimsIdentity)(context.HttpContext.User.Identity ??
+                throw new Exception("User identity is null"));
+
+            string adminClaim = identity.FindFirst("admin")?.Value ??
+                throw new Exception("No admin claim value");
+
+            string subIdClaim = identity.FindFirst("sub_id")?.Value ??
+                throw new Exception("No subject id claim value");
+
+            var isAdmin = bool.Parse(adminClaim);
+            var subjectId = int.Parse(subIdClaim);
+            int affectedUserId = context.ActionArguments["id"] as int? ??
+                throw new Exception("No id parameter");
+
+            if (subjectId != affectedUserId && !isAdmin)
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
