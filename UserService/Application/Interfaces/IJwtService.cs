5d1a271be0f9bd995ae8ae30a44f184bdb2db85f
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-12 17:41:43 +0300

    Add JwtService

---
+++ UserService/Application/Interfaces/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Application.Interfaces
+{
+    public interface IJwtService
+    {
+        public string GetJwtToken(User user);
+    }
+}
---
+++ UserService/Application/Services/JwtService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using UserService.Application.Interfaces;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.Services
+{
+    public class JwtService(RsaSecurityKey key, JwtSecurityTokenHandler handler) : IJwtService
+    {
+        public string GetJwtToken(User user)
+        {
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                Audience = "User",
+                Issuer = "UserService",
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new("sub_id", user.UserId.ToString(CultureInfo.InvariantCulture)),
+                    new("admin", user.Role.HasAdminPrivileges.ToString(CultureInfo.InvariantCulture))
+                }),
+                SigningCredentials = credentials
+            };
+
+            var token = handler.CreateToken(tokenDescriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
