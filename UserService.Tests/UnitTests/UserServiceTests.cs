b9e9fb9be1c6c0fccb2a875782267b3b47659349
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-17 18:31:32 +0300

    DeleteUser and GetUser unit tests

--- UserService.Tests/UnitTests/UserServiceTests.cs
+++ UserService.Tests/UnitTests/UserServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Routing;
 using Moq;
+using UserService.Application.DTOs;
 using UserService.Application.Interfaces;
 using UserService.Application.Options;
 using UserService.Application.Requests;
@@ -29,29 +30,34 @@ namespace UserService.Tests.UnitTests
                 );
         }
 
+        private static User CreateUser(string email = "email", string name = "John Doe",
+            string? passwordHash = null, bool emailConfirmed = false,
+            int id = 1) => new()
+            {
+                Email = email,
+                Name = name,
+                PasswordHash = passwordHash!,
+                Role = null!,
+                IsEmailConfirmed = emailConfirmed,
+                UserId = id
+            };
+
         [Test]
         public static void CreateUser_UserWithConfirmedEmailExists()
         {
             // Arrange
             Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
 
-            var user = new User()
-            {
-                Email = "email",
-                Name = "name",
-                PasswordHash = "passwordhash",
-                Role = null!,
-                IsEmailConfirmed = true
-            };
+            var user = CreateUser(email: "email", emailConfirmed: true);
 
-            mockUserRepo.Setup(m => m.GetUserByEmailAsync("email").Result)
+            mockUserRepo.Setup(m => m.GetUserByEmailAsync(user.Email).Result)
                 .Returns(user);
 
             var userService = CreateUserService(mockUserRepo.Object);
 
             var req = new CreateUserReq()
             {
-                Email = "email",
+                Email = user.Email,
                 Name = "John Doe",
                 Password = "12345"
             };
@@ -60,7 +66,7 @@ namespace UserService.Tests.UnitTests
             Assert.ThrowsAsync<EmailInUseException>(() => userService.CreateUserAsync(req));
 
             // Assert
-            mockUserRepo.Verify(m => m.GetUserByEmailAsync("email"), Times.Once());
+            mockUserRepo.Verify(m => m.GetUserByEmailAsync(user.Email), Times.Once());
         }
 
         [Test]
@@ -70,15 +76,9 @@ namespace UserService.Tests.UnitTests
             Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
             Mock<LinkGenerator> mockLinkGen = new();
 
-            var user = new User()
-            {
-                Email = "email",
-                Name = "name",
-                PasswordHash = "passwordhash",
-                Role = null!
-            };
+            var user = CreateUser(email: "email");
 
-            mockUserRepo.Setup(m => m.GetUserByEmailAsync("email").Result)
+            mockUserRepo.Setup(m => m.GetUserByEmailAsync(user.Email).Result)
                 .Returns(user);
             mockUserRepo.Setup(m => m.UpdateUserAsync(user))
                 .Returns(Task.CompletedTask);
@@ -90,7 +90,7 @@ namespace UserService.Tests.UnitTests
 
             var req = new CreateUserReq()
             {
-                Email = "email",
+                Email = user.Email,
                 Name = "John Doe",
                 Password = "12345"
             };
@@ -99,7 +99,7 @@ namespace UserService.Tests.UnitTests
             var dto = userService.CreateUserAsync(req).Result;
 
             // Assert
-            mockUserRepo.Verify(m => m.GetUserByEmailAsync("email"), Times.Once());
+            mockUserRepo.Verify(m => m.GetUserByEmailAsync(user.Email), Times.Once());
             mockUserRepo.Verify(m => m.UpdateUserAsync(user), Times.Once());
 
             Assert.Multiple(() =>
@@ -116,16 +116,10 @@ namespace UserService.Tests.UnitTests
             Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
             Mock<LinkGenerator> mockLinkGen = new();
 
-            var user = new User()
-            {
-                Email = "email",
-                Name = "John Doe",
-                PasswordHash = null!,
-                Role = null!
-            };
+            var user = CreateUser(email: "email", name: "Bob");
 
             mockUserRepo.Setup(m => m.GetUserByEmailAsync(user.Email).Result)
-                .Returns<IUserRepository, User>(null);
+                .Returns((User?)null);
             mockUserRepo.Setup(m => m.CreateUserAsync(It.IsAny<User>()).Result)
                 .Returns(user);
 
@@ -146,13 +140,109 @@ namespace UserService.Tests.UnitTests
             var dto = userService.CreateUserAsync(req).Result;
 
             // Assert
-            mockUserRepo.Verify(m => m.GetUserByEmailAsync("email"), Times.Once());
+            mockUserRepo.Verify(m => m.GetUserByEmailAsync(req.Email), Times.Once());
             mockUserRepo.Verify(m => m.CreateUserAsync(It.IsAny<User>()), Times.Once());
 
             Assert.Multiple(() =>
             {
                 Assert.That(dto.Name, Is.EqualTo(user.Name));
                 Assert.That(dto.Email, Is.EqualTo(user.Email));
+            });
+        }
+
+        [Test]
+        public static void DeleteUser_NoUserExists()
+        {
+            // Arrange
+            Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
+
+            const int userId = 1;
+
+            mockUserRepo.Setup(m => m.GetUserAsync(userId).Result)
+                .Returns((User?)null);
+
+            var userService = CreateUserService(mockUserRepo.Object);
+
+            // Act
+            Assert.ThrowsAsync(Is.TypeOf<UserNotFoundException>()
+                               .And.Message.Contains(userId.ToString()), 
+                () => userService.DeleteUserAsync(userId));
+
+            // Assert
+            mockUserRepo.Verify(m => m.GetUserAsync(userId), Times.Once());
+        }
+
+        [Test]
+        public static void DeleteUser_UserExists()
+        {
+            // Arrange
+            Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
+
+            var user = CreateUser();
+
+            mockUserRepo.Setup(m => m.GetUserAsync(user.UserId).Result)
+                .Returns(user);
+            mockUserRepo.Setup(m => m.DeleteUserAsync(user.UserId))
+                .Returns(Task.CompletedTask);
+
+            var userService = CreateUserService(mockUserRepo.Object);
+
+            // Act
+            userService.DeleteUserAsync(user.UserId).Wait();
+
+            // Assert
+            mockUserRepo.Verify(m => m.GetUserAsync(user.UserId), Times.Once());
+            mockUserRepo.Verify(m => m.DeleteUserAsync(user.UserId), Times.Once());
+        }
+
+        [Test]
+        public static void GetUser_NoUserExists()
+        {
+            // Arrange
+            Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
+
+            const int userId = 1;
+
+            mockUserRepo.Setup(m => m.GetUserAsync(userId).Result)
+                .Returns((User?)null);
+
+            var userService = CreateUserService(mockUserRepo.Object);
+
+            // Act
+            Assert.ThrowsAsync(Is.TypeOf<UserNotFoundException>()
+                               .And.Message.Contains(userId.ToString()),
+                () => userService.GetUserAsync(userId));
+            
+            // Assert
+            mockUserRepo.Verify(m => m.GetUserAsync(userId), Times.Once());
+        }
+
+        [Test]
+        public static void GetUser_UserExists()
+        {
+            // Arrange
+            Mock<IUserRepository> mockUserRepo = new(MockBehavior.Strict);
+
+            var user = CreateUser();
+
+            mockUserRepo.Setup(m => m.GetUserAsync(user.UserId).Result)
+                .Returns(user);
+
+            var userService = CreateUserService(mockUserRepo.Object);
+
+            // Act
+            UserDTO dto = userService.GetUserAsync(user.UserId).Result;
+
+            // Assert
+            mockUserRepo.Verify(m => m.GetUserAsync(user.UserId), Times.Once());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(dto.UserId, Is.EqualTo(user.UserId));
+                Assert.That(dto.Name, Is.EqualTo(user.Name));
+                Assert.That(dto.Email, Is.EqualTo(user.Email));
+                Assert.That(dto.CreatedOn, Is.EqualTo(user.CreatedOn));
+                Assert.That(dto.Role, Is.EqualTo(user.Role));
             });
         }
     }
