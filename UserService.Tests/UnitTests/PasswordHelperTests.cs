5123bd60cbd95b99886e953f453f95419ced7793
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-20 15:21:02 +0300

    Add PasswordHelper tests

---
+++ UserService.Tests/UnitTests/PasswordHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using UserService.Application.Services;
+
+namespace UserService.Tests.UnitTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public static class PasswordHelperTests
+    {
+        private static readonly PasswordHelper helper = new();
+
+        [Test]
+        public static void ValidHash()
+        {
+            // Arrange
+            const string password = "SomePassword";
+
+            // Act
+            string hash = helper.HashPassword(password);
+
+            // Assert
+            bool res = helper.IsValid(password, hash);
+
+            Assert.That(res);
+        }
+
+        [Test]
+        public static void InvalidHash()
+        {
+            bool res = helper.IsValid("SomePassword", helper.HashPassword("OtherPassword"));
+
+            Assert.That(!res);
+        }
+    }
+}
