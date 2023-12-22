2395d3c1c07544de6f221fb2aaf376bfa4bc41b9
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-12-22 15:05:10 +0300

    Setup for UserService integration tests

---
+++ UserService.Tests/IntegrationTests/UserServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Images;
+using Microsoft.Extensions.Logging;
+
+namespace UserService.Tests.IntegrationTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self)]
+    public static class UserServiceTests
+    {
+        private static IFutureDockerImage serviceImage;
+        private static IContainer serviceContainer;
+
+        [OneTimeSetUp]
+        public static async Task Setup()
+        {
+            var factory = LoggerFactory.Create(o => o.AddDebug());
+            TestcontainersSettings.Logger = factory.CreateLogger("logger");
+
+            serviceImage = new ImageFromDockerfileBuilder()
+                .WithName("userservicetest")
+                .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), "UserService")
+                .WithDockerfile("Dockerfile")
+                .Build();
+
+            await serviceImage.CreateAsync();
+
+            serviceContainer = new ContainerBuilder()
+                .Build();
+
+            await serviceContainer.StartAsync();
+        }
+
+        [Test]
+        public static void UnauthenticatedResponses()
+        {
+            
+        }
+
+        [OneTimeTearDown]
+        public static async Task TearDown()
+        {
+            await serviceImage.DisposeAsync();
+
+            if (serviceContainer != null)
+            {
+                await serviceContainer.DisposeAsync();
+            }
+        }
+    }
+}
